beec9c90cafae25f3544fb5cc071c4e0b046c679
iuryalvez <iuryalves774@gmail.com>
2023-02-22 23:26:56 -0300

    mapeamento objeto-relacional Professor-Turma

---
+++ pegaqui/src/main/java/com/tfd/pegaqui/entities/Professor.java
@@ -0,0 +1,61 @@
+// pacote "ENTITIES": é dessa forma que o spring identifica uma classe que 
+// será utilizada para suas funções de mapeamento objeto-relacional
+
+package com.tfd.pegaqui.entities;
+
+// import java.io.File;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.JoinTable;
+import jakarta.persistence.OneToMany;
+// import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+// identifica a classe como entidade para o mapeamento objeto-relacional
+@Entity 
+@Table(name = "tb_professores") // o modelo relacional é uma tabela e o nome dela no banco de dados do JPA é tb_professores
+public class Professor {
+	
+	@Id // identificando que é a chave primária do banco
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // função que gera um valor auto incrementável no banco de dados
+	private int id; // id encapsulado
+
+	private String nome;
+
+    // Relacionamento entre Professor e Turma
+	@OneToMany // um professor para muitas turmas
+	@JoinColumn(name = "turmas_id") // sabendo que o modelo é uma tabela, adicionamos uma nova coluna para mostrar as turmas_id relacionadas
+    // mostra de acordo com a chave estrangeira associada a turmas_id
+	private List<Turma> turmas; // cada professor tem uma lista de turmas que ele pode ministrar ou já ter ministrado
+	
+	// private List<Publicacao> avaliacao; // Construtor 1
+	private float notaMedia;
+	// static int idCriado = 0;
+	
+	Professor(String nome){ // construtor
+		// this.id = idCriado;
+		// idCriado++;
+		this.nome = nome;
+		this.turmas = null;
+		// this.avaliacao = null;
+		this.notaMedia = 0;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getNotaMedia() {
+		return notaMedia;
+	}
+	public void setNotaMedia(float notaMedia) {
+		this.notaMedia = notaMedia;
+	}
+}
---
+++ pegaqui/src/main/java/com/tfd/pegaqui/entities/Turma.java
@@ -0,0 +1,79 @@
+// pacote "ENTITIES": é dessa forma que o spring identifica uma classe que 
+// será utilizada para suas funções
+
+package com.tfd.pegaqui.entities;
+
+// import java.io.File;
+// import java.util.List;
+
+// Escolher os imports com base na especificação para manter o projeto funcional
+// Nesse caso não importar o que há implementação 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+// identifica a classe para o mapeamento objeto-relacional (JPA)
+@Entity 
+@Table(name = "tb_turmas") // o modelo relacional é uma tabela e o nome dela no banco de dados do JPA é tb_turmas
+public class Turma {
+	
+	@Id // vai ser a chave primária do banco
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // valor auto-incrementável no banco de dados
+	private int id;
+	
+	private String nome;
+
+    // Relacionamento entre Turma e Professor:
+	@ManyToOne // muitas turmas para um professor, cada professor possui muitas turmas, cada turma possui 1 professor
+	@JoinColumn(name = "professores_id") // sabendo que o modelo é uma tabela, adicionamos uma nova coluna para mostrar o professor_id relacionado
+	// mostra de acordo com a chave estrangeira associada a turmas_id
+    private Professor professor;
+	
+	private String semestre;
+    
+    // private List<Publicacao> avaliacoes; // Construtor 1
+	// private List<Publicacao> publicacoes; // Construtor 2
+	private float notaMedia;
+	// private List<Publicacao> publicacoesForum; // Construtor 2
+	// static int idCriado = 0;
+	// static int numAvaliacoes = 0;
+	// static int numPublicacoes = 0;
+	// static int numPublicacoesForum = 0;
+	
+	Turma(String nome, String semestre, Professor professor){
+		this.nome = nome;
+		this.semestre = semestre;
+		this.professor = professor;
+		// this.avaliacoes = null;
+		// this.publicacoes = null;
+		this.notaMedia = 0;
+		// this.publicacoesForum = null;
+		// this.id = idCriado;
+		// idCriado++;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public float getNotaMedia() {
+		return notaMedia;
+	}
+	public void setNotaMedia(float notaMedia) {
+		this.notaMedia = notaMedia;
+	}
+	public String getSemestre() {
+        return semestre;
+    }
+    public void setSemestre(String semestre) {
+        this.semestre = semestre;
+    }
+	
+}
