3392045d6d56ef2063a9e4e47f78071eb6f971fb
iuryalvez <iuryalves774@gmail.com>
2023-02-22 22:26:20 -0300

    criacao do projeto - spring initializr

---
+++ pegaqui/src/main/java/com/tfd/pegaqui/PegaquiApplication.java
@@ -0,0 +1,13 @@
+package com.tfd.pegaqui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PegaquiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PegaquiApplication.class, args);
+	}
+
+}
---
+++ pegaqui/src/test/java/com/tfd/pegaqui/PegaquiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tfd.pegaqui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PegaquiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
